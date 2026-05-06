451dcedc71b250d3ca037b8eb4921e30c7481800
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2026-05-06 16:03:19 -0300

    Mutação

---
+++ src/main.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+"""
+                PROBLEMA 1
+
+    Minimizar f(x) = Σ xi^2,
+    para i de 1 a 10,
+    onde 
+        -5 ≤ x ≤ 5
+"""
+
+
+N = 5 #tamanho da população
+D = 10 #numero de variaveis
+F = 1.5 #Fator de Escala (0, 2], magnitude do passo
+CR = 0.7 #Prob. de Cruzamento CR
+
+def evolucao_diferencial():
+    
+    populacao = inicia_populacao()
+    print(populacao)
+
+    custos = np.array([avaliar_fitness(x) for x in populacao])
+
+    iteracoes = 0
+
+    while iteracoes < 1000:
+        
+        # Mutação
+        for i in range(N):
+            x_i = populacao[i]
+            indices = list(range(N))
+            indices.remove(i)
+            
+            xr1, xr2, xr3 = populacao[np.random.choice(indices, 3, replace=False)]
+            
+            v = xr1 + F * (xr2 - xr3)
+
+            v = np.clip(v, -5, 5)
+
+
+       
+        # Cruzamento
+        # Recombinação
+        # Seleção
+
+        iteracoes += 1
+
+
+# inicia população: conjunto de varios vetores candidados a x
+def inicia_populacao():
+    p = np.random.uniform(-5, 5, (N, D))
+    return np.round(p, 2)
+
+
+# verificar qualidade da solução: quanto mais baixo melhor
+def avaliar_fitness(x):
+    return np.sum(x**2)
+
+
+evolucao_diferencial()
